9c2ddff6cf125bf89810153dd4640db75223c823
Daniil Istomin <istomin.05.3@gmail.com>
2025-10-11 21:50:50 +0700

    Initial commit

---
+++ src/test/java/org/example/marketplace/CodbidMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodbidMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
